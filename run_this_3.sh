678b5c8584e11ebcecb798d0df2e0873dc70596a
redsphinx <gabi.ras@hotmail.com>
2016-03-29 16:02:11 +0200

    SMO run files

--- run_this_3.sh
+++ run_this_3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1 
-#SBATCH -t 04:00:00 
-java -cp "./lib/*" meka.classifiers.multilabel.CDN -t data/train_2att.arff -W weka.classifiers.trees.RandomForest > output/only_train_2att_7.txt
+#SBATCH -t 07:00:00 
+java -cp "./lib/*" meka.classifiers.multilabel.CCq -t data/train_2att.arff -W weka.classifiers.functions.SMO > output/only_train_2att_6.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CDN -t data/train_2att.arff -W weka.classifiers.functions.SMO > output/only_train_2att_7.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CDT -t data/train_2att.arff -W weka.classifiers.functions.SMO > output/only_train_2att_8.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CT -t data/train_2att.arff -W weka.classifiers.functions.SMO > output/only_train_2att_9.txt
+java -cp "./lib/*" meka.classifiers.multilabel.DBPNN -t data/train_2att.arff -W weka.classifiers.functions.SMO > output/only_train_2att_10.txt
 # sleep 300
--- run_this_5.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-#SBATCH -N 1 
-#SBATCH -t 06:00:00 
-java -cp "./lib/*" meka.classifiers.multilabel.CDT -t data/train_2att.arff -W weka.classifiers.trees.RandomForest > output/only_train_2att_8.txt
-java -cp "./lib/*" meka.classifiers.multilabel.CDT -t data/train_3att.arff -W weka.classifiers.trees.RandomForest > output/only_train_3att_8.txt
-# sleep 300
-
