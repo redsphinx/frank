46f0167369567b868a7b4a171865e7744e2cfaa0
redsphinx <gabi.ras@hotmail.com>
2016-04-11 15:15:03 +0200

    new test and train files

--- run_this.sh
+++ run_this.sh
@@ -1,10 +1,6 @@
 #!/bin/bash 
 #SBATCH -N 1 
 #SBATCH -t 07:00:00 
-java -cp "./lib/*" meka.classifiers.multilabel.BCC -t data/train_3att.arff -W weka.classifiers.bayes.BayesNet > output/only_train_3att_1.txt
-java -cp "./lib/*" meka.classifiers.multilabel.BPNN -t data/train_3att.arff -W weka.classifiers.bayes.BayesNet > output/only_train_3att_2.txt
-java -cp "./lib/*" meka.classifiers.multilabel.BR -t data/train_3att.arff -W weka.classifiers.bayes.BayesNet > output/only_train_3att_3.txt
-java -cp "./lib/*" meka.classifiers.multilabel.BRq -t data/train_3att.arff -W weka.classifiers.bayes.BayesNet > output/only_train_3att_4.txt
-java -cp "./lib/*" meka.classifiers.multilabel.CC -t data/train_3att.arff -W weka.classifiers.bayes.BayesNet > output/only_train_3att_5.txt
+java -cp "./lib/*" meka.classifiers.multilabel.BCC -t data/train255.arff -W weka.classifiers.bayes.BayesNet > output/manyFeatures/output255.txt
 # sleep 300
 
