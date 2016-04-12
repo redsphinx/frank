303af845654cc3c577a8c652f2efd799d76dd13e
redsphinx <gabi.ras@hotmail.com>
2016-04-12 02:04:08 +0200

    mistake in the test data file

--- run_this_4.sh
+++ run_this_4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 #SBATCH -N 1 
 #SBATCH -t 07:00:00 
-java -cp "./lib/*" meka.classifiers.multilabel.BR -t data/train255.arff -T data/test255.arff -W weka.classifiers.trees.RandomForest > output/245_randomforest_3.txt
-java -cp "./lib/*" meka.classifiers.multilabel.CC -t data/train255.arff -T data/test255.arff -W weka.classifiers.trees.RandomForest > output/245_randomforest_5.txt
-java -cp "./lib/*" meka.classifiers.multilabel.CDN -t data/train255.arff -T data/test255.arff -W weka.classifiers.trees.RandomForest > output/245_randomforest_7.txt
-java -cp "./lib/*" meka.classifiers.multilabel.CDT -t data/train255.arff -T data/test255.arff -W weka.classifiers.trees.RandomForest > output/245_randomforest_8.txt
-java -cp "./lib/*" meka.classifiers.multilabel.CT -t data/train255.arff -T data/test255.arff -W weka.classifiers.trees.RandomForest > output/245_randomforest_9.txt
+java -cp "./lib/*" meka.classifiers.multilabel.BR -t data/train255.arff -W weka.classifiers.trees.RandomForest -T data/test255.arff > output/245_randomforest_3.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CC -t data/train255.arff -W weka.classifiers.trees.RandomForest -T data/test255.arff > output/245_randomforest_5.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CDN -t data/train255.arff -W weka.classifiers.trees.RandomForest -T data/test255.arff > output/245_randomforest_7.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CDT -t data/train255.arff -W weka.classifiers.trees.RandomForest -T data/test255.arff > output/245_randomforest_8.txt
+java -cp "./lib/*" meka.classifiers.multilabel.CT -t data/train255.arff -W weka.classifiers.trees.RandomForest -T data/test255.arff > output/245_randomforest_9.txt
 # sleep 300
